3a06bf2b1f3e99f6d7d48961491db9b5c5d279c6
MicPingui <martincernaibarra@hotmail.com>
2022-11-03 21:14:19 -0300

    se añade packageJson

--- tests/Feature/BooksApiTest.php
+++ tests/Feature/BooksApiTest.php
@@ -2,20 +2,70 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Book;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class BooksApiTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_the_application_returns_a_successful_response()
-    {
-        $response = $this->get('/');
+class BooksApiTest extends TestCase {
+    use RefreshDatabase;
+    /** @test */
+    function can_get_all_books(){
+        $books = Book::factory(4)->create();
 
-        $response->assertStatus(200);
+        // route('books.index') por si la ruta llegase a cambiar
+        $response = $this->getJson(route('books.index'));
+        $response->assertJsonFragment([
+            'title'=>$books[0]->title
+        ]);
+    }
+
+    /** @test */
+    function can_get_one_book(){
+        $book = Book::factory()->create();
+
+        $response = $this->getJson(route('books.show', $book));
+        $response->assertJsonFragment([
+            'title'=>$book->title
+        ]);
+    }
+
+    /** @test */
+    function can_create_books(){
+        $this->postJson(route('books.store'),[])->assertJsonValidationErrorFor('title');
+
+        $this->postJson(route('books.store'),[
+           'title'=>'libro de prueba 01'
+        ])->assertJsonFragment([
+            'title'=>'libro de prueba 01'
+        ]);
+
+        $this->assertDatabaseHas('books',[
+            'title'=>'libro de prueba 01'
+        ]);
+    }
+
+    /** @test */
+    function can_update_books(){
+        $book = Book::factory()->create();
+
+        $this->putJson(route('books.update', $book),[])->assertJsonValidationErrorFor('title');
+
+        $this->putJson(route('books.update', $book),[
+            'title'=>'Libro editado'
+        ])->assertJsonFragment([
+            'title'=>'Libro editado'
+        ]);
+
+        $this->assertDatabaseHas('books',[
+            'title'=>'Libro editado'
+        ]);
+    }
+
+    /** @test */
+    function can_delete_books(){
+        $book = Book::factory()->create();
+        $this->deleteJson(route('books.destroy',$book))->assertNoContent();
+
+        $this->assertDatabaseCount('books',0);
     }
 }
